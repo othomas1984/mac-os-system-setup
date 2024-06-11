7b0c15588b26ba863659038eeffb4e94310272bd
Owen Thomas <owen.thomas@gotinder.com>
2024-06-11 11:59:17 -0700

    Add homebrew step

--- setup_brew.sh
+++ setup_brew.sh
@@ -3,6 +3,8 @@
 if ! command -v brew &> /dev/null
 then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/owenthomas/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew bundle
